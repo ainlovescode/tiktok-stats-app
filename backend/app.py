8e54c506aabbe8e48dc1036147581c968bc6fe7a
Ainul Md Razib <ainul.mdrazib@thoughtworks.com>
2021-04-16 23:50:01 +0800

    #3 [Ain] Implement tiktok analytics helper class in flask server app

--- backend/app.py
+++ backend/app.py
@@ -1,10 +1,31 @@
-from flask import Flask
+from gevent import monkey
+
+monkey.patch_all()
+
+from flask import Flask, make_response
+from flask import request, jsonify
+
+from backend.helper.ttapi import TTApi
+
 app = Flask(__name__)
+ttApi = TTApi()
 
 
 @app.route('/')
 def hello():
     return "Hello World!"
 
+
+@app.route('/analytics/<username>', methods=['GET'])
+def getAnalytics(username):
+    if request.method == 'GET':
+        ttApi.setUser(username)
+        analytics_response = ttApi.getAnalytics()
+        return make_response(jsonify(isError=False,
+                                     message="Success",
+                                     statusCode=200,
+                                     data=analytics_response), 200)
+
+
 if __name__ == '__main__':
-    app.run()
+    app.run(host='0.0.0.0')
