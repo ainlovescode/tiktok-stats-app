7363e41fb880c9f0fc739e812c09110182cbc2fb
Ainul Md Razib <ainul.mdrazib@thoughtworks.com>
2021-04-17 00:33:07 +0800

    #5 [Ain] Update metrics display overview component with test to accept props

--- frontend/src/components/MetricsDisplay/MetricsDisplayOverview/MetricsDisplayOverview.js
+++ frontend/src/components/MetricsDisplay/MetricsDisplayOverview/MetricsDisplayOverview.js
@@ -1,25 +1,22 @@
 import "./MetricsDisplayOverview.css"
 import profile_photo from "../../../mocks/avatar.png"
 
-export default function MetricsDisplayOverview () {
+export default function MetricsDisplayOverview (props) {
     return (
         <div className="metrics-display__overview">
             <div className="user__profile-photo">
                 <img src={profile_photo} width="64px" alt="profile of tiktok user"/>
             </div>
             <div className="user__username">
-                <h3>Username</h3>
+                <h3>{props.username}</h3>
                 Sample Name
             </div>
             <hr/>
             <div className="user__metrics" display="table">
                 <div className="user-profile" display="table-row">
-                    <div className="bold user-profile__rate">13.0%</div>
+                    <div className="bold user-profile__rate">
+                        {props.profile_rate} %</div>
                     <em>Profile Engagement Rate</em>
-                </div>
-                <div className="user-post"  display="table-row">
-                    <div className="bold user-post__rate">25.0%</div>
-                    <em>Average Post Engagement Rate</em>
                 </div>
             </div>
         </div>
--- frontend/src/components/MetricsDisplay/MetricsDisplayOverview/MetricsDisplayOverview.test.js
+++ frontend/src/components/MetricsDisplay/MetricsDisplayOverview/MetricsDisplayOverview.test.js
@@ -2,12 +2,13 @@ import {shallow, mount} from 'enzyme'
 import MetricsDisplayOverview from "./MetricsDisplayOverview";
 
 describe("MetricsDisplayOverview", function () {
-    it("should render profile photo, username, and engagement metrics", function () {
-        const wrapper = shallow(<MetricsDisplayOverview/>);
+    it("should render profile photo, username, and engagement metrics with sample response data", function () {
+        const fakeUsername = "myusername"
+        const fakeProfileRate = "123.0"
+        const wrapper = shallow(<MetricsDisplayOverview username={fakeUsername} profile_rate={fakeProfileRate}/>);
 
         expect(wrapper.exists(".user__profile-photo")).toBe(true);
-        expect(wrapper.exists(".user__username")).toBe(true);
-        expect(wrapper.exists(".user-profile")).toBe(true);
-        expect(wrapper.exists(".user-post")).toBe(true);
+        expect(wrapper.find(".user__username").text()).toMatch(fakeUsername);
+        expect(wrapper.find(".user-profile").text()).toMatch(fakeProfileRate);
     })
 })
