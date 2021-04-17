1980f9aa9ad06b75158d96dbebf6356e16f412a6
Ainul Md Razib <ainul.mdrazib@thoughtworks.com>
2021-04-17 18:43:06 +0800

    #5 [Ain] Change profile eng rate prop name in metrics display

--- frontend/src/components/MetricsDisplay/MetricsDisplay.js
+++ frontend/src/components/MetricsDisplay/MetricsDisplay.js
@@ -1,9 +1,13 @@
 import MetricsDisplayOverview from "./MetricsDisplayOverview/MetricsDisplayOverview";
+import React from 'react';
+
 
 export default function MetricsDisplay(props) {
+
     return (
         <div className="metrics-display">
-            <MetricsDisplayOverview username={props.username} profileData={props.profileData}/>
+            <MetricsDisplayOverview username={props.username}
+                                    profileEngRate={props.analyticsResponse.data["profile_eng_rate"]}/>
         </div>
     )
 }
--- frontend/src/components/MetricsDisplay/MetricsDisplay.test.js
+++ frontend/src/components/MetricsDisplay/MetricsDisplay.test.js
@@ -1,16 +1,19 @@
 import {mount, shallow} from 'enzyme'
 import MetricsDisplay from "./MetricsDisplay";
+import fakeAnalyticsResponse from "../../mocks/fake_analytics_response.json"
 
 describe("MetricsDisplay", function () {
-    const fakeProps = {
-        "username": "fakeUsername",
-        "profileEngRate":"123.00%"
-    }
-    it("should render overview component and pass props to overview", function (){
-        const wrapper = shallow(<MetricsDisplay username={fakeProps.username} profileData={fakeProps.profileEngRate}/>);
+
+    const fakeUsername = "fakeUsername";
+
+    it("should render overview component and pass analytics props to overview", function (){
+        const fakeProfileEngRate = fakeAnalyticsResponse.data.profile_eng_rate;
+        const fakePostMetadataAll = fakeAnalyticsResponse.data.post_metadata_all
+
+        const wrapper = shallow(<MetricsDisplay username={fakeUsername} analyticsResponse={fakeAnalyticsResponse}/>);
 
         expect(wrapper.exists("MetricsDisplayOverview")).toBe(true);
-        expect(wrapper.find("MetricsDisplayOverview").prop("username")).toBe(fakeProps.username);
-        expect(wrapper.find("MetricsDisplayOverview").prop("profileData")).toBe(fakeProps.profileEngRate);
+        expect(wrapper.find("MetricsDisplayOverview").prop("username")).toBe(fakeUsername);
+        expect(wrapper.find("MetricsDisplayOverview").prop("profileEngRate")).toBe(fakeProfileEngRate);
     })
 })
