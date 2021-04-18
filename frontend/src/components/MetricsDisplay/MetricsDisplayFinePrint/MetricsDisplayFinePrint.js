c53124779bd2c7e690cc0a1d597947a25fe9accf
Ainul Md Razib <ainul.mdrazib@thoughtworks.com>
2021-04-19 01:58:11 +0800

    #8 [Ain] Improve styling of metrics fine print

--- frontend/src/components/MetricsDisplay/MetricsDisplayFinePrint/MetricsDisplayFinePrint.js
+++ frontend/src/components/MetricsDisplay/MetricsDisplayFinePrint/MetricsDisplayFinePrint.js
@@ -3,27 +3,35 @@ import React from 'react';
 export default function MetricsDisplayFinePrint() {
     return (
         <div className="metrics-display-fineprint">
+            <hr/>
             <div className={"fineprint profile-eng"}>
                 <sup>1</sup>Profile engagement rate is calculated as follows:
                 <br/>
+                <em>
                 (Total Likes + Comments + Shares for the 20 most recent posts)
                 <br/>
-                divided by
+                --- divided by ---
                 <br/>
-                (Follower count × 20)
+                (Follower count × 20)</em>
             </div>
             <div className={"fineprint average-views"}>
-                <sup>2</sup>Average views per TikTok is calculated by
-                finding the average number of views for the 20 most recent posts
+                <sup>2</sup>Average views per TikTok is calculated by:
+                <br/>
+                <em>Total views for the 20 most recent posts
+                <br/>
+                --- divided by ---
+                <br/>
+                20</em>
             </div>
             <div className={"fineprint post-eng"}>
-                <sup>3</sup>Post engagement rate is calculated as follows for <strong>each</strong>:
+                <sup>3</sup>Post engagement rate is calculated as follows for <strong>each post</strong>:
                 <br/>
-                Likes + Comments + Shares
+
+                <em>Likes + Comments + Shares
                 <br/>
-                divided by
+                --- divided by ---
                 <br/>
-                Views
+                Views</em>
             </div>
         </div>
     )
