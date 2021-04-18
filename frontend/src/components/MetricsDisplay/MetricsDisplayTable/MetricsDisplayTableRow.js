eb45b1b24ad3d935c527f3e0d9d7c186559d16ce
Ainul Md Razib <ainul.mdrazib@thoughtworks.com>
2021-04-19 01:11:23 +0800

    #7 [Ain] Switch positions of caption and post e.r. table headings

--- frontend/src/components/MetricsDisplay/MetricsDisplayTable/MetricsDisplayTableRow.js
+++ frontend/src/components/MetricsDisplay/MetricsDisplayTable/MetricsDisplayTableRow.js
@@ -3,8 +3,8 @@ import React from 'react'
 export default function MetricsDisplayTableRow(props) {
     return (
         <tr className={"metrics-display__table-row"}>
-            <td className={"metrics-display__table-row__caption"}>{props.postMetadata["caption"]}</td>
             <td className={"metrics-display__table-row__post-rate"}>{props.postMetadata["post_eng_rate"]}</td>
+            <td className={"metrics-display__table-row__caption"} >{props.postMetadata["caption"]}</td>
         </tr>
 
     )
