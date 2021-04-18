eb4c9d038fb69a617dd32683b63dd138725ea689
Ainul Md Razib <ainul.mdrazib@thoughtworks.com>
2021-04-19 01:14:03 +0800

    #7 [Ain] Apply relevant styling to metrics display component

--- frontend/src/components/MetricsDisplay/MetricsDisplay.js
+++ frontend/src/components/MetricsDisplay/MetricsDisplay.js
@@ -1,15 +1,14 @@
 import MetricsDisplayOverview from "./MetricsDisplayOverview/MetricsDisplayOverview";
 import React from 'react';
 import MetricsDisplayTable from "./MetricsDisplayTable/MetricsDisplayTable";
-
+import "./MetricsDisplay.css"
 
 export default function MetricsDisplay(props) {
-
     return (
         <div className="metrics-display">
             <MetricsDisplayOverview userInfo={props.userInfo}
                                     analytics={props.analytics}/>
-            <MetricsDisplayTable postMetadataAll={props.analytics.post_metadata_all}/>
+            <MetricsDisplayTable postMetadataAll={props.analytics["post_metadata_all"]}/>
         </div>
     )
 }
